d10d5d2b084a21f3b0605bdd7cb5616c2db16ebb
Hamsternik <hamsternik9@gmail.com>
2014-07-25 22:50:49 +0300

    Make an Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,37 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+### In this function we gain a put (or don't) a matrix
+### And have a four "public" methods - special functions for next working with data
+### func. "setMatrix" set a matrix if it doesn't gain in func.arguments
+### func. "getMatrix" - print our matrix
+### func. "setSolveMatrix" - take a "inversible" matrix
+### func. "getSolveMatrix" - print input "inversible" matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+   m <- NULL
+   setMatrix <- function(NewMatrix) {
+      x <<- NewMatrix
+      m <<- NULL
+   } 
+   getMatrix <- function() x
+   setSolveMatrix <- function(SolveMatrix) m <<- SolveMatrix
+   getSolveMatrix <- function() m
+   list(setMatrix = setMatrix, getMatrix = getMatrix, setSolveMatrix = setSolveMatrix, getSolveMatrix = getSolveMatrix)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+### In this func. we take our object x (inctance, if talk with OOP concepts)
+### We check our solve matrix - does it exist or not
+### and give a message if we caching our solve matrix
+### or not and find a solve matrix, after that caching it
+cacheSolve <- function(x) {
+   m <- x$getSolveMatrix()
+   if (!is.null(x$getSolveMatrix())) {
+      message("getting cached data")
+      return(m)
+   }
+   Matrix <- x$getMatrix()
+   m <- solve(x$setSolveMatrix(Matrix))
+   print(m)
 }
